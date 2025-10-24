451bbd1fe215bede299ebaff82506c23b24fed60
Vadim <vadimchubok@gmail.com>
2025-10-24 13:53:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,17 +4,15 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
-        nonlocal cache_storage
-        res = (args,)
+    def wrapper(*args, **kwargs) -> int:
         key = (args, tuple(kwargs.items()))
         if key in cache_storage:
             print("Getting from cache")
-            res = cache_storage[key]
+            return cache_storage[key]
         else:
             print("Calculating new result")
-            res = func(*args)
-            cache_storage[key] = res
-        return res
+            result = func(*args, **kwargs)  # ✅ pass kwargs correctly
+            cache_storage[key] = result
+            return result
 
     return wrapper
